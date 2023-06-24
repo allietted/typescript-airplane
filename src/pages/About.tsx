87a9c7603c6525a6087c4c8a6d118b9189570142
Alliette Dortelus <allietted@gmail.com>
2023-06-23 21:59:00 -0400

    added page layout

---
+++ src/pages/About.tsx
@@ -0,0 +1,20 @@
+import Address from '../components/AddressBook/AddressBook';
+import AppLayout from '../layout/AppLayout';
+
+
+const About = () => {
+    return(
+        <>
+          <AppLayout
+            title="About Title">
+            <h2>About Content</h2>
+            <h2>Address</h2>
+            <Address
+                street="street name"
+                unit={1337}
+                zip={1234567234242343}
+                state="CA"/>
+            </AppLayout>
+        </>
+    )
+}
